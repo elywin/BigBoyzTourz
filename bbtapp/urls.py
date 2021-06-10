658fc12875bfdd7b44c8aeeaf72b9cb358ee488f
elywin <lukeeraelywin@gmail.com>
2021-06-10 17:00:23 +0300

    loaded static files for contact page created contact logic and set its url, and loaded static files for contact page

--- bbtapp/urls.py
+++ bbtapp/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 
 urlpatterns = [    
     path('', views.index, name='index'),
-    path('/about', views.about, name='about'),
+    path('about', views.about, name='about'),
+    path('contact', views.contact, name='contact'),
+
 ]
